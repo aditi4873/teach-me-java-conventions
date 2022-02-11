ce2a4a16a68d978af4142d74d149058ab9f8ff48
atharvi <atharvimali2018@gmail.com>
2022-02-11 15:30:23 +0530

    edited PowerFinder.java

---
+++ src/powerpackage/PowerFinder.java
@@ -0,0 +1,13 @@
+package powerpackage;
+
+public class PowerFinder {
+    public int power(int base, int exponent) 
+    {
+        int result = 1;
+        for (int i = 0; i<exponent; i++) 
+        {
+            result *= base;
+        }
+        return result;
+    }
+}
